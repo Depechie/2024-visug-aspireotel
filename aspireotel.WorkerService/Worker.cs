548fca05d6bf55b157b95143453c0e66fee0a455
Glenn Versweyveld <glenn.versweyveld@gmail.com>
2024-10-12 16:35:59 +0200

    Add RabbitMQ - part 2

--- aspireotel.WorkerService/Worker.cs
+++ aspireotel.WorkerService/Worker.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics;
 using System.Text;
 using aspireotel.QueueCommon;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -7,6 +10,9 @@ namespace aspireotel.WorkerService;
 
 public class Worker : BackgroundService
 {
+    private static readonly ActivitySource _activitySource = new("Aspire.RabbitMQ.Client");
+    private static readonly TextMapPropagator Propagator = Propagators.DefaultTextMapPropagator;
+    
     private readonly ILogger<Worker> _logger;
     private readonly IServiceProvider _serviceProvider;
     private IConnection? _messageConnection;
@@ -47,10 +53,44 @@ public class Worker : BackgroundService
     private async Task ProcessMessageAsync(object? sender, BasicDeliverEventArgs args)
     {
         _logger.LogInformation($"Processing message...");
-        
-        var body = args.Body.ToArray();
-        var message = Encoding.UTF8.GetString(body);
 
-        _logger.LogInformation($"Message received: {message}");
+        var parentContext = Propagator.Extract(default, args.BasicProperties, ExtractTraceContextFromBasicProperties);
+        Baggage.Current = parentContext.Baggage;
+
+        using var activity = _activitySource.StartActivity($"{Queue.Orders} consume", ActivityKind.Consumer, parentContext.ActivityContext);
+        if (activity is not null)
+        {
+            AddActivityTags(activity);
+            var body = args.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            _logger.LogInformation($"Message received: {message}");
+        }
     }
+
+    private IEnumerable<string> ExtractTraceContextFromBasicProperties(IBasicProperties props, string key)
+    {
+        try
+        {
+            if (props.Headers.TryGetValue(key, out var value))
+            {
+                var bytes = value as byte[];
+                return new[] { bytes is null ? "" : Encoding.UTF8.GetString(bytes) };
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Failed to extract trace context: {ex}");
+        }
+
+        return Enumerable.Empty<string>();
+    }
+
+    private void AddActivityTags(Activity activity)
+    {
+        activity?.SetTag("messaging.system", "rabbitmq");
+        activity?.SetTag("messaging.destination_kind", "queue");
+        activity?.SetTag("messaging.destination", Queue.Orders);
+        activity?.SetTag("messaging.rabbitmq.routing_key", Queue.Orders);
+    }    
 }
